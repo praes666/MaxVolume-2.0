c8735c24e35c8f9f4705ce61bc9a2e5151388dc5
praes666 <praesvip@mail.ru>
2024-11-27 10:48:22 +0300

    start inv

--- frontend/src/components/trackContainerBig.jsx
+++ frontend/src/components/trackContainerBig.jsx
@@ -1,10 +1,27 @@
+import { GoPlay } from "react-icons/go";
+import { FaHeart } from "react-icons/fa6";
+import { CiCircleMore } from "react-icons/ci";
+
+import { useState } from 'react'
 import '../styles/TrackContainerBig.css'
 
 export default function TrackContainerBig({ img, name, author }){
+
+    const [isMove, setIsMove] = useState(false);
+
     return(
         <div className="trackContainerBig">
-            <div className="trackImg">
-                <img className="trackImg" src={img}/>
+            <div className="trackImg" onMouseEnter={() => setIsMove(true)} onMouseLeave={() => setIsMove(false)}>
+                <img src={img}/>
+                <div className='imgeHover' style= {{
+                    display: isMove ? "block" : "none"
+                }}>
+                    <GoPlay className='hoverPlayButton'/>
+                    <div className="hoverButtons">
+                        <FaHeart className='hoverLikeButton'/>
+                        <CiCircleMore className='hoverMoreButton'/>
+                    </div>
+                </div>
             </div>
             <div className="trackInfo">
                 <button className='trackName'>
