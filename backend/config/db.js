d511eb227fdc1116287cb2b1e2e166eae0009bfb
praes666 <praes13@mail.ru>
2025-02-07 14:31:25 +0300

    гитигнор + вебшторм

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./users.db')
+const db = new sqlite3.Database('../users.db')
 
 db.serialize(() => {
     db.run(`CREATE TABLE IF NOT EXISTS users (
